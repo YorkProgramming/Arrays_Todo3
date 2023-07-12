011f7398216f7871b68752912f81db69a7d9ff05
Matthew York <myorkprogramming@gmail.com>
2023-07-11 23:57:33 -0700

    first commit

---
+++ index.js
@@ -0,0 +1,7 @@
+//1. 
+// Remove Negatives
+
+/* Implement removeNegatives() that accepts an array, 
+removes negative values, and returns the same array (not a copy), 
+preserving non-negatives’ order. As always, do not use built-in array 
+functions. */
