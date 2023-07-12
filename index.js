5af79b69df9f50d6f2503fb8d4088e3c74b2d4f4
Matthew York <myorkprogramming@gmail.com>
2023-07-12 00:17:32 -0700

    Second to Last

--- index.js
+++ index.js
@@ -24,3 +24,25 @@ function removeNegatives(arr){
 }
 
 console.log(removeNegatives([1,2,-3,4,-5,6,-7,8,-9,10]));
+
+
+//#############################################################################
+
+//2.
+// Second-to-Last
+
+/* Return the second-to-last element of an array. Given [42,true,4,"Liam",7],
+    return "Liam". If array is too short, return null. */
+
+function secondToLast(arr){
+    if (arr.length < 2){
+        return null;
+    }
+    else{
+        return arr[arr.length - 2];
+    }
+}
+
+console.log(secondToLast([42,true,4,"Liam",7]));
+
+//#############################################################################
