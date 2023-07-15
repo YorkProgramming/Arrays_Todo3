496dfef64267fbd5e11456a78d6ea5e8ddb4b6ae
Matthew York <myorkprogramming@gmail.com>
2023-07-14 21:41:31 -0700

    Skyline Heights

--- index.js
+++ index.js
@@ -126,3 +126,14 @@ function nthLargest(arr, n){
 console.log(nthLargest([5,2,3,6,4,9,7],3));
 
 //#############################################################################
+
+//6.
+// Skyline Heights
+
+/* Lovely Burbank has a breathtaking view of the Los Angeles skyline. Let’s say
+    you are given an array with heights of consecutive buildings, starting closest
+    to you and extending away. Array [-1,7,3] would represent three buildings:
+    first is actually out of view below street level, behind it is second at 7
+    stories high, third is 3 stories high (hidden behind the 7-story). You are
+    situated at street level. Return array containing heights of buildings you
+    can see, in order. Given [-1,1,1,7,3] return [1,7]. */
